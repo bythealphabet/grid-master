7c6a6f44d49772110924c832c13516443ce6875b
Isaac lucas <isaaclucas@pop-os.localdomain>
2024-03-01 08:39:03 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs()],
+});
